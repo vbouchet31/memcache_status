4a30fa04cbf6caaa2cda04022398f6e7f87e1d69
vbouchet <vincent.bouchet.31@gmail.com>
2023-06-20 10:14:47 +0200

    Improve the cache item view page.

--- src/Controller/ItemView.php
+++ src/Controller/ItemView.php
@@ -3,15 +3,13 @@
 namespace Drupal\memcache_status\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
 use Drupal\memcache\Driver\MemcacheDriverFactory;
 use Drupal\memcache_status\MemcacheStatusHelper;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ItemView extends ControllerBase {
 
-  /**
-   * ModalFormContactController constructor.
-   */
   public function __construct(MemcacheDriverFactory $memcacheDriverFactory, MemcacheStatusHelper $memcache_status_helper) {
     $this->memcacheDriverFactory = $memcacheDriverFactory;
     $this->memcacheStatusHelper = $memcache_status_helper;
@@ -38,18 +36,50 @@ class ItemView extends ControllerBase {
     /** @var $memcache \Drupal\memcache\DrupalMemcacheInterface */
     $memcache = $this->memcacheDriverFactory->get($bin);
 
-    $data = $memcache->getMemcache()->get(urlencode($key));
+    $data = $memcache->getMemcache()->get(urlencode(rawurldecode($key)));
 
     if ($data) {
       return [
-        '#markup' => '<pre>' . print_r($data, TRUE) . '</pre>',
+        [
+          '#theme'  => 'table',
+          '#header' => [],
+          '#rows'   => [
+            ['cid', $data->cid],
+            ['created', $data->created],
+            ['expire', $data->expire],
+            ['tags', ['data' => [
+              '#theme' => 'item_list',
+              '#items' => $data->tags,
+            ]]],
+            ['checksum', $data->checksum],
+            ['data', [ 'data' => [
+              '#markup' => '<pre>' . var_export($data->data, TRUE) . '</pre>',
+            ]]],
+          ],
+        ],
+        [
+          '#type' => 'link',
+          '#title' => $this->t('Edit'),
+          // @TODO: Use Route instead.
+          '#url' => Url::fromUri('base:/admin/reports/memcache-status/item/' . $key . '/edit'),
+          '#attributes' => [
+            'class' => ['button', 'button--primary'],
+          ],
+        ],
+        [
+          '#type' => 'link',
+          '#title' => $this->t('Flush'),
+          // @TODO: Use Route instead.
+          '#url' => Url::fromUri('base:/admin/reports/memcache-status/item/' . rawurlencode($key) . '/flush'),
+          '#attributes' => [
+            'class' => ['button', 'button--secondary'],
+          ],
+        ],
       ];
     }
 
     return [
       '#markup' => $this->t('Invalid key'),
     ];
-
-
   }
 }
